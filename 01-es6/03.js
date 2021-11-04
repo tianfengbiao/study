d10da2772756ad6cf5504bb53e4e7544f52e8eaa
tianfengbiao <18208648965@163.com>
2021-11-05 00:21:50 +0800

    去掉删除目录

--- 01-es6/03.js
+++
@@ -1,7 +0,0 @@
-var path = require('path');
-
-console.log('__dirname=====',__dirname)
-console.log('ath.resolve()11===',path.resolve('/aa','/bb'))
-console.log('ath.resolve()22===',path.resolve('/aa','./bb'))
-console.log('ath.resolve()33===',path.resolve('./aa','./bb'))
-console.log('ath.join()===',path.join(__dirname,'./aa','./bb'))
