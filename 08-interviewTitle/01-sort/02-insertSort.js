0caf7c4c71cf0aaefb45ffedee752530639fbabd
田风标 <18208648965@163.com>
2022-03-04 11:33:38 +0800

    Update 02-insertSort.js

--- 08-interviewTitle/01-sort/02-insertSort.js
+++ 08-interviewTitle/01-sort/02-insertSort.js
@@ -22,3 +22,28 @@ const res = insertSort(arr);
 
 
 console.log('res555==',res);
+
+
+
+var arr1 = [3,1,2,4,8,6,7,5];
+
+function insertSortTest(oldArr) {
+    var newArr = [oldArr[0]];// 默认在老牌里取第一个放到新牌里
+    for(var i = 1; i < oldArr.length; i++) {
+        var oldB = oldArr[i];// 旧牌里的一张牌与新牌一一对比大小
+        var j = 0;
+        while(j < newArr.length) {
+            if(oldB < newArr[j]) {// newArr=[1,2,3]是一个有序数组；
+                break;
+            }
+            j++;// 找到插入位置
+        }
+        newArr.splice(j, 0, oldB);
+    }
+
+    return newArr;
+}
+debugger
+var result = insertSortTest(arr1);
+
+console.log('result===', result);
