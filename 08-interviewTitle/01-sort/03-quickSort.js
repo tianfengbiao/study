d7b2256bf71502611a52b183a681d4d92e42f54b
田风标 <18208648965@163.com>
2022-03-07 11:30:30 +0800

    Update 03-quickSort.js

--- 08-interviewTitle/01-sort/03-quickSort.js
+++ 08-interviewTitle/01-sort/03-quickSort.js
@@ -1,5 +1,5 @@
 
-let arr = [5,2,1,3,4,6]
+let arr = [5,2,1,3,4,5,7,6]
 
 const quickSort = (arr) => {
     if(arr.length <= 1) {
@@ -18,10 +18,38 @@ const quickSort = (arr) => {
         }
         i++;
     }
-    return quickSort(leftList).concat(midVal, rightList);
+    return quickSort(leftList).concat(midVal, quickSort(rightList));
 }
 const res = quickSort(arr);
 
 // debugger
 
 console.log('res555==',res);
+
+
+var arr1 = [4,2,1,3,5,4,7,6,8];
+
+function quickSort1(arr) {
+    if(arr.length <= 1) {
+        return arr;
+    }
+    var l = arr.length;
+    var midInd = Math.floor(l / 2);
+    var midVal = arr.splice(midInd, 1);
+    var leftArr = [];
+    var righArr = [];
+    for(var i = 0; i < arr.length; i++) {
+        if(midVal >= arr[i]) {
+            leftArr.push(arr[i])
+        } else {
+            righArr.push(arr[i])
+        }
+    }
+    return quickSort1(leftArr).concat(midVal,quickSort1(righArr));
+}
+debugger
+
+var res1 = quickSort1(arr1);
+
+
+console.log('res1111==',res1);
