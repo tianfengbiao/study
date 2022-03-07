52c87dba841904092046f2f06f7b15aa6f05e5a5
田风标 <18208648965@163.com>
2022-03-07 11:45:34 +0800

    Update 04-mergeSort.js

--- 08-interviewTitle/01-sort/04-mergeSort.js
+++ 08-interviewTitle/01-sort/04-mergeSort.js
@@ -29,3 +29,32 @@ const res = mergeSort(arr1,arr2);
 
 
 console.log('res555==',res);
+
+
+
+var arr3 = [1,2,7,9]// 有序数组
+var arr4 = [3,4,5,6,8]// 有序数组
+
+function mergeSort1(arr1, arr2) {
+    var newArr = [];
+    while(arr1.length && arr2.length) {
+        if(arr1[0] < arr2[0]) {
+            newArr.push(arr1.shift())
+        } else {
+            newArr.push(arr2.shift())
+        }
+    }
+
+    if(arr1.length) {
+        newArr.concat(arr1)
+    }
+    if(arr2.length) {
+        newArr.concat(arr2)
+    }
+
+    return newArr;
+}
+
+var res3 = mergeSort1(arr3, arr4);
+debugger
+console.log('res33333===',res3)
