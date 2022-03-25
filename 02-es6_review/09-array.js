37dbfefcb5b174fcf84a9d2f8a8679949fde8367
田风标 <18208648965@163.com>
2022-03-25 09:59:06 +0800

    Create 09-array.js

---
+++ 02-es6_review/09-array.js
@@ -0,0 +1,83 @@
+
+// Array.from方法用于将两类对象转为真正的数组：类似数组的对象（array-like object）和可遍历（iterable）的对象（包括 ES6 新增的数据结构 Set 和 Map）。
+
+// 下面是一个类似数组的对象，Array.from将它转为真正的数组。
+
+let arrayLike = {
+    '0': 'a',
+    '1': 'b',
+    '2': 'c',
+    length: 3
+};
+
+// ES5的写法
+var arr1 = [].slice.call(arrayLike); // ['a', 'b', 'c']
+
+// ES6的写法
+let arr2 = Array.from(arrayLike); // ['a', 'b', 'c']
+
+
+
+// Array.of()方法用于将一组值，转换为数组。
+
+Array.of(3, 11, 8) // [3,11,8]
+Array.of(3) // [3]
+Array.of(3).length // 1
+
+
+
+// 数组实例的find方法，用于找出第一个符合条件的数组成员。
+// 它的参数是一个回调函数，所有数组成员依次执行该回调函数，直到找出第一个返回值为true的成员，然后返回该成员。如果没有符合条件的成员，则返回undefined。
+
+[1, 4, -5, 10].find((n) => n < 0)  // -5
+
+
+// fill方法使用给定值，填充一个数组。
+new Array(3).fill(7)
+// [7, 7, 7]
+
+
+// ES6 提供三个新的方法——entries()，keys()和values()——用于遍历数组。
+// 它们都返回一个遍历器对象（详见《Iterator》一章），可以用for...of循环进行遍历，
+// 唯一的区别是keys()是对键名的遍历、values()是对键值的遍历，entries()是对键值对的遍历。
+
+for (let index of ['a', 'b'].keys()) {
+  console.log(index);
+}
+// 0
+// 1
+
+for (let elem of ['a', 'b'].values()) {
+  console.log(elem);
+}
+// 'a'
+// 'b'
+
+for (let [index, elem] of ['a', 'b'].entries()) {
+  console.log(index, elem);
+}
+// 0 "a"
+// 1 "b"
+
+
+// Array.prototype.includes方法返回一个布尔值，表示某个数组是否包含给定的值，与字符串的includes方法类似。ES2016 引入了该方法。
+
+[1, 2, 3].includes(2)     // true
+[1, 2, 3].includes(4)     // false
+[1, 2, NaN].includes(NaN) // true
+
+[NaN].indexOf(NaN)
+// -1
+
+
+// 数组的成员有时还是数组，
+// Array.prototype.flat()用于将嵌套的数组“拉平”，变成一维的数组。该方法返回一个新数组，对原数据没有影响。
+
+[1, 2, [3, 4]].flat()
+// [1, 2, 3, 4]
+
+
+const arr = [5, 12, 8, 130, 44];
+arr.at(2) // 8
+arr.at(-2) // 130
+// 如果参数位置超出了数组范围，at()返回undefined
