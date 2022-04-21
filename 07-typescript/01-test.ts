dea96b0ed5b68b14d787395edb46bf5afc43fe98
田风标 <18208648965@163.com>
2022-04-21 16:09:24 +0800

    Update 01-test.ts

--- 07-typescript/01-test.ts
+++ 07-typescript/01-test.ts
@@ -1,1 +1,117 @@
-console.log('hello world');
+// 布尔值
+// 最基本的数据类型就是简单的true/false值，在JavaScript和TypeScript里叫做boolean（其它语言中也一样）。
+
+let isDone: boolean = false;
+
+// number
+let decLiteral: number = 6;
+let hexLiteral: number = 0xf00d;
+let binaryLiteral: number = 0b1010;
+let octalLiteral: number = 0o744;
+
+// string
+let name1: string = "bob";
+name1 = 'test'
+
+// 数组
+
+let list: number[] = [1, 2, 3];
+
+let list1: Array<number> = [1, 2, 3];
+
+
+
+// 元组 Tuple
+// Declare a tuple type
+let x: [string, number];
+// Initialize it
+x = ['hello', 10]; // OK
+
+
+// enum
+// 默认情况下，从0开始为元素编号。 你也可以手动的指定成员的数值。 例如，我们将上面的例子改成从 1开始编号：
+enum Color {Red = 1, Green, Blue}
+let colorName: string = Color[2];
+
+console.log(colorName);  // 显示'Green'因为上面代码里它的值是2
+
+// Any
+let notSure: any = 4;
+notSure = "maybe a string instead";
+notSure = false; // okay, definitely a boolean
+
+
+
+// Void
+// 某种程度上来说，void类型像是与any类型相反，它表示没有任何类型。 当一个函数没有返回值时，你通常会见到其返回值类型是 void：
+
+function warnUser(): void {
+    console.log("This is my warning message");
+}
+// 声明一个void类型的变量没有什么大用，因为你只能为它赋予undefined和null：
+
+let unusable: void = undefined;
+
+
+// Null 和 Undefined
+// TypeScript里，undefined和null两者各自有自己的类型分别叫做undefined和null。 和 void相似，它们的本身的类型用处不是很大：
+
+// Not much else we can assign to these variables!
+let u: undefined = undefined;
+let n: null = null;
+
+
+// Never
+// never类型表示的是那些永不存在的值的类型。 
+// 例如， never类型是那些总是会抛出异常或根本就不会有返回值的函数表达式或箭头函数表达式的返回值类型； 
+// 变量也可能是 never类型，当它们被永不为真的类型保护所约束时。
+
+// 返回never的函数必须存在无法达到的终点
+function error(message: string): never {
+    throw new Error(message);
+}
+
+// 推断的返回值类型为never
+function fail() {
+    return error("Something failed");
+}
+
+// 返回never的函数必须存在无法达到的终点
+function infiniteLoop(): never {
+    while (true) {
+    }
+}
+
+
+
+// Object
+// object表示非原始类型，也就是除number，string，boolean，symbol，null或undefined之外的类型。
+
+// 使用object类型，就可以更好的表示像Object.create这样的API。例如：
+
+declare function create(o: object | null): void;
+
+create({ prop: 0 }); // OK
+create(null); // OK
+
+
+
+
+
+// 类型断言
+// 有时候你会遇到这样的情况，你会比TypeScript更了解某个值的详细信息。 通常这会发生在你清楚地知道一个实体具有比它现有类型更确切的类型。
+
+// 通过类型断言这种方式可以告诉编译器，“相信我，我知道自己在干什么”。 类型断言好比其它语言里的类型转换，但是不进行特殊的数据检查和解构。 
+// 它没有运行时的影响，只是在编译阶段起作用。 TypeScript会假设你，程序员，已经进行了必须的检查。
+
+// 类型断言有两种形式。 其一是“尖括号”语法：
+
+let someValue1: any = "this is a string";
+
+let strLength: number = (<string>someValue1).length;
+// 另一个为as语法：
+
+let someValue2: any = "this is a string";
+
+let strLength1: number = (someValue2 as string).length;
+
